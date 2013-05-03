35079bb90097859577e9be00a98bc0f8cb9e6952
Christoph Stasch <staschc@uni-muenster.de>
2013-05-03 09:11:23 +0200

    clickovers hinzugefuegt; Verwendung von JQuery

--- js/application.js
+++ js/application.js
@@ -1,4 +1,145 @@
-function refreshMainPanel(){
+var panelIDs = ["StartPanel","TheoriesPanel","AddPanel","BrowsePanel","InfoPanel","ContactPanel"];
 
-	alert("Hello World!" + this.tagname);
+
+/*
+*************************************
+***JQUERY METHODS
+*************************************
+*/
+function showAddPanel(){
+	$("#NavList").children().attr("class","");
+	$("#NavAdd").attr("class","active");
+	setPanelActive("AddPanel");
 }
+
+/**
+*  method for setting the panel on active
+*
+* @param activePanelID {string} the identifier of the panel that should be active
+* @exception if the panel is not contained in the panelIDs configuration
+*/
+function setPanelActive(activePanelID){
+	if (arrayContains(panelIDs,activePanelID)){
+		for (x in panelIDs){
+			panelID=panelIDs[x];
+			if (panelID!=activePanelID){
+				$("#"+panelID).css("display","none");
+			}
+			else {
+				$("#"+activePanelID).css("display","inline");
+			}
+		}
+	}
+	else {
+		throw "Panel is not activated or defined!";
+	}
+}
+
+function showHomePanel(){
+var navList = document.getElementById("NavList");
+setNavigationActive(navList.getElementsByTagName("li"), "NavHome");
+setPanelActive("StartPanel");
+}
+
+function showInfoPanel(){
+	var navList = document.getElementById("NavList");
+	setNavigationActive(navList.getElementsByTagName("li"),"NavInfo");
+	setPanelActive("InfoPanel");
+}
+
+
+function showBrowsePanel(){
+	var navList = document.getElementById("NavList");
+	setNavigationActive(navList.getElementsByTagName("li"),"NavBrowse");
+	setPanelActive("BrowsePanel");
+}
+
+function showTheoriesPanel(){
+	var navList = document.getElementById("NavList");
+	setNavigationActive(navList.getElementsByTagName("li"),"NavTheories");
+	setPanelActive("TheoriesPanel");
+}
+
+function showContactPanel(){
+	var navList = document.getElementById("NavList");
+	setNavigationActive(navList.getElementsByTagName("li"),"NavContact");
+	setPanelActive("ContactPanel");
+}
+
+/*
+* funtion for setting a navigation item active
+*
+*  	@param listItems - the nodelist containing the li elements of the navigation panel
+*	@param activeItemID - the ID of the navigation item that should be active
+*/
+function setNavigationActive(listItems, activeItemID){
+	if (1<listItems.length) {
+		for (x in listItems){
+			var listItem = listItems[x];
+			if (listItem.id==activeItemID){
+				listItem.className="active"
+			} else {
+				listItem.className="";
+			}
+		}
+	}
+	else {
+		throw "Method resetClassnames is only defined for NodeList with more than one element!";
+	}
+
+}
+
+function arrayContains(a, obj) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Create an instance of an entry which represents an entry in the statitics portal
+ *
+ * @constructor
+ * @this {Entry}
+ * @param  {string} sourceURL
+ * @param  {string} dataType
+ * @param  {string} license
+ * @param  {string} phenomenon
+ * @param  {bbox} obsWindow
+ */
+function Entry(sourceURL,dataType,license,phenomenon,obsWindow){
+	this.sourceURL=sourceURL;
+	this.dataType=dataType;
+	this.license=license;
+	this.phenomenon=phenomenon;
+	this.obsWindow=obsWindow;
+
+
+	/**
+	 * serialises the entry object to RDF
+	 * 
+	 * @return {string} string containing the RDF output
+	 */
+	function toRDF(){
+		var rdfOutput;
+		return rdfOutput;
+
+	}
+}
+
+/**
+ * Creates a Bbox object that represents observed window
+ *
+ * @constructor
+ * @this{Bbox}
+ * @param  {number} minx
+ * @param  {number} miny
+ * @param  {number} maxx
+ * @param  {number} maxy
+ */
+function Bbox(minx,miny,maxx,maxy){
+
+}
+
